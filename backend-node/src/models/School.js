1c0299a21d28c707f4c0f742e481f1bc0b138460
Greice Paz <greicepazvargas@gmail.com>
2019-11-23 19:10:54 -0300

    Criação de get e post da entidade school.

---
+++ backend-node/src/models/School.js
@@ -0,0 +1,69 @@
+const {Schema,model} = require('mongoose');
+
+const SchoolSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    }
+    ,thumbmail:{
+        type:String,
+        required:true
+    },
+    type:{
+        type:String,
+        required:true
+    },
+    state:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    district:{
+        type:String,
+        required:true
+    },
+    morning:{
+        type:Boolean,
+        default:false
+    },
+    evening:{
+        type:Boolean,
+        default:false
+    },
+    night:{
+        type:Boolean,
+        default:false
+    },
+    extra_curricular:{
+        type:Boolean,
+        default:false
+    },
+    kitchen:{
+        type:Boolean,
+        default:false
+    },
+    quadra:{
+        type:Boolean,
+        default:false
+    },
+    laboratorio:{
+        type:Boolean,
+        default:false
+    },
+    biblioteca:{
+        type:Boolean,
+        default:false
+    },
+    labciencia:{
+        type:Boolean,
+        default:false
+    },
+},{
+    timestamps:true   
+}
+);
+
+module.exports = model('School', SchoolSchema);
--- backend-node/src/routes.js
+++ backend-node/src/routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const routes = express.Router();
 const QuestionController = require('./controllers/QuestionController');
 const UserController = require('./controllers/UserController');
+const SchoolController = require('./controllers/SchoolController');
 
 routes.post('/question',  QuestionController.store);
 routes.get('/questions',  QuestionController.index);
 routes.post('/user',  UserController.store);
 routes.get('/users',  UserController.index);
+routes.get('/schools', SchoolController.index);
+routes.post('/school', SchoolController.store);
 
 module.exports = routes;
