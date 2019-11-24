bb66075a02c03807b2ccb00ef8c8b04f5eb23e02
Lucas Santos <lucas.santos@southsystem.com.br>
2019-11-24 09:20:16 -0200

    Wip: SchoolDetail Component.

--- frontend/pages/escolas/[schoolId].js
+++ frontend/pages/escolas/[schoolId].js
@@ -1,32 +1,40 @@
 import React from "react";
 import Head from "next/head";
+import { SchoolDetail } from "../../components/SchoolDetail";
 
 import Layout from "../../components/MyLayout";
 
 import api from "../../services/api";
 
+
 const School = ({ school }) => {
-  console.log(school);
+
   return (
-    <Layout>
-      <Head>
-        <title>{school.name}</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link href="/styles.css" rel="stylesheet" />
-      </Head>
-      <main></main>
-    </Layout>
+    <>
+      <Layout>
+        <Head>
+          <title>{school.name}</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link href="/styles.css" rel="stylesheet" />
+        </Head>
+        <main>
+          <SchoolDetail school={school}>
+          </SchoolDetail>
+        </main>
+
+      </Layout>
+
+    </>
   );
 };
 
 School.getInitialProps = async ctx => {
   const response = await api.get("/schools", {
     params: {
-      id: ctx.query.schoolId
+      _id: ctx.query.schoolId
     }
   });
-  console.log(response);
-  return { school: response.data };
+  return { school: response.data[0] };
 };
 
 export default School;
